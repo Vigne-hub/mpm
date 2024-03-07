24aea244b8496c0c2b97765723a24242f465ef6c
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-03-07 17:54:22 -0500

    Fixed hooks to run python 3.8: communicate needs str encoded to bytes print statements updated runtimeerror statements updated path isfile -> is_file()

--- mpm/hooks.py
+++ mpm/hooks.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def on_plugin_install(plugin_directory, ostream=sys.stdout):
-    '''
+    """
     Run ``on_plugin_install`` script for specified plugin directory (if
     available).
 
@@ -22,17 +22,16 @@ def on_plugin_install(plugin_directory, ostream=sys.stdout):
         File system to plugin directory.
     ostream :file-like
         Output stream for status messages (default: ``sys.stdout``).
-    '''
+    """
     current_directory = os.getcwd()
 
-    plugin_directory = ph.path(plugin_directory).realpath()
-    print >> ostream, ('Processing post-install hook for: '
-                        '{}'.format(plugin_directory.name))
+    plugin_directory = ph.path(plugin_directory)
+    print(f'Processing post-install hook for: {plugin_directory.name}', file=ostream)
 
     hooks_dir_i = plugin_directory.joinpath('hooks/Windows').realpath()
     hook_path_i = hooks_dir_i.joinpath('on_plugin_install.bat')
 
-    if hook_path_i.isfile():
+    if hook_path_i.is_file():
         logger.info('Processing post-install hook for: %s',
                     plugin_directory.name)
         os.chdir(hook_path_i.parent)
@@ -41,13 +40,11 @@ def on_plugin_install(plugin_directory, ostream=sys.stdout):
                                stdin=sp.PIPE)
             # Emulate return key press in case plugin uses
             # "Press <enter> key to continue..." prompt.
-            process.communicate(input='\r\n')
+            process.communicate(input='\r\n'.encode())
             if process.returncode != 0:
-                raise RuntimeError('Process return code == {}'
-                                   .format(process.returncode))
+                raise RuntimeError(f'Process return code == {process.returncode}')
             return hook_path_i
         except Exception as exception:
-            raise RuntimeError('Error running: {}\n{}'.format(hook_path_i,
-                                                              exception))
+            raise RuntimeError(f'Error running: {hook_path_i}\n{exception}')
         finally:
             os.chdir(current_directory)
