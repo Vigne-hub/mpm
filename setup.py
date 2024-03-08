828afef9e773f399424cf79b9e2e1c49f3fdab3d
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-03-08 14:48:56 -0500

    Update pacakging

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import sys
 import setuptools as st
 sys.path.insert(0, '.')
 
-install_requires = ['configobj', 'path-helpers',
+install_requires = ['microdrop_ext_libs', 'path-helpers',
                     'progressbar2', 'pyyaml', 'si-prefix>=0.4.post3']
 
 if platform.system() == 'Windows':
     install_requires += ['pywin32']
 
 st.setup(name='microdrop-plugin-manager',
-         version="0.8.3.alpha",
+         version="0.8.4.alpha",
          description='MicroDrop plugin manager.',
          keywords='',
          author='Christian Fobel',
          author_email='christian@fobel.net',
          url='https://github.com/wheeler-microfluidics/mpm',
          license='BSD',
-         packages=['mpm', 'pip_helpers'],
+         packages=['mpm', ],
          install_requires=install_requires,
          # Install data listed in `MANIFEST.in`
          include_package_data=True,
